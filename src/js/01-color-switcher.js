d527e363663253d7fe6f9ec65e088a89f1a301d8
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-08 22:18:29 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startBtn.addEventListener('click', clickBtnStart);
+
+stopBtn.addEventListener('click', clickBtnStop);
+
+let timerId = null;
+stopBtn.disabled = true;
+
+function clickBtnStart() {
+  setBodyBgColor();
+
+  timerId = setInterval(() => {
+    setBodyBgColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function clickBtnStop() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  document.body.style.backgroundColor = '';
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBodyBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}

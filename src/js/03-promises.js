6359854ce7b92126e1fe8e481dd1bc49c028ff35
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-11 16:56:04 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', formSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+  const amount = +formEl.elements.amount.value;
+  const firstDelay = +formEl.elements.delay.value;
+  const stepDelay = +formEl.elements.step.value;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, firstDelay + (i - 1) * stepDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
